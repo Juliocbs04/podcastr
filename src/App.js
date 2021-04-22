5520834d8871a48c7a82c24290066c9167d1a0d0
Juliocbs04 <profile.julio@gmail.com>
2021-04-22 20:26:39 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Button from "./Button";
+
+
+function App() {
+  return (
+    <>
+      <Button>Botao 1</Button>
+      <Button title='Botao 2'></Button>
+      <Button title='Botao 3'></Button>
+      <Button title='Botao 4'></Button>
+      <Button title='Botao 5'></Button>
+
+    </>
+  );
+}
+
+export default App;
